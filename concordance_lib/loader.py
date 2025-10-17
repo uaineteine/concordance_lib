1be2ee974cca204bb6219cc84992c523c2e7a733
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2025-10-17 16:19:30 +1100

    create spine concordance code

--- concordance_lib/loader.py
+++ concordance_lib/loader.py
@@ -1,8 +1,8 @@
 from .pathing import *
 from .spineprd import SpineProduct
-from .linkprjs import LinkageProjects
 
 from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import countDistinct
 
 def load_ent_map(id_group_num:int, sparkSession: SparkSession) -> DataFrame:
     path = get_maps_path(id_group_num)
@@ -60,3 +60,33 @@ def load_spine_product(spine_prd:SpineProduct, sparkSession:SparkSession) -> Dat
     path = spine_prd.get_spine_path()
     
     return sparkSession.read.parquet(path)
+
+def create_spine_conc(idGroup:int, projectIDs: list[int], spine_prd:SpineProduct, sparkSession:SparkSession) -> DataFrame:
+    proj_df = load_linkage_results(idGroup, sparkSession)
+    
+    #filter for targetted project IDs
+    proj_df = proj_df.filter(proj_df["PROJECT_ID"].isin(projectIDs))
+    
+    #filter for spine version ID
+    proj_df = proj_df.filter(proj_df["SPINE_VERSION_ID"] == spine_prd.spine_version.spine_version)
+    
+    #rename the linkage projects columns
+    proj_df = proj_df.withColumnRenamed("FROM_SYN_AEUID", "SYNHETIC_AEUID")\
+                     .withColumnRenamed("TO_SPINE_ID", "SPINE_ID")
+    proj_df = proj_df.select("SYNTHETIC_AEUID", "SPINE_ID")
+    
+    #load an existing spine product
+    spine_df = load_spine_product(spine_prd, sparkSession)
+    spine_df = spine_df.select("SYNTHETIC_AEUID", "SPINE_ID")
+    
+    #append these in union
+    combined_df = spine_df.unionByName(proj_df).dropDuplicates()
+    
+    multi_spineid = combined_df.groupBy("SYNTHETIC_AEUID").agg(countDistinct("SPINE_ID").alias("spine_id_linked_count"))
+    n_violations = multi_spineid.filter("spine_id_linked_count>1").count()
+    
+    if n_violations > 0:
+        raise ValueError(f"Found {n_violations} violations of spine ID linkage.")
+    
+    return combined_df
+    
