0464bd816154f1c688441ce34ef24aec7d7df7ec
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2025-10-17 17:05:30 +1100

    test spark session created

--- unit_tester.py
+++ unit_tester.py
@@ -1,7 +1,8 @@
 print("Running test module")
 
 from concordance_lib import create_spine_conc
+from pyspark.sql import SparkSession
 
-
+spark = SparkSession.builder.appName("testSession").getOrCreate()
 
 print("Test finished")
