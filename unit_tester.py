92edfc57191fd3a767a7bb4d8083c4431111e843
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2025-10-17 17:21:25 +1100

    test to produce spine concordance

--- unit_tester.py
+++ unit_tester.py
@@ -1,8 +1,13 @@
 print("Running test module")
 
-from concordance_lib import create_spine_conc
+from concordance_lib import create_spine_conc, SpineProduct, SpineProductVersion, LinkageProjects
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("testSession").getOrCreate()
+
+#make spine product
+ver = SpineProductVersion(8, 1)
+prjids = LinkageProjects.get_linkage_projects_in_range(1, 20)
+prod = create_spine_conc(1, prjids, SpineProduct("air", ver), spark)
 
 print("Test finished")
