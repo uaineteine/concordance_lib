3dc796505adb190b72d50ead81a5b6737c915637
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2025-10-18 15:12:29 +1100

    improved test

--- unit_tester.py
+++ unit_tester.py
@@ -6,8 +6,8 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("testSession").getOrCreate()
 
 #make spine product
-ver = SpineProductVersion(8, 1)
+prd_info = SpineProduct("air", SpineProductVersion(8, 1))
 prjids = LinkageProjects.get_linkage_projects_in_range(1, 20)
-prod = create_spine_conc(1, prjids, SpineProduct("air", ver), spark)
+prod = create_spine_conc(1, prjids, prd_info, spark)
 
 print("Test finished")
