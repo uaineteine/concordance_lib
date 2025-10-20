5665faabc40da1a18c975bbe7586cf164d818839
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-10-20 16:58:20 +1100

    update package name

--- conclib/setup.py
+++ conclib/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='concordance_lib',
+    name='conclib',
     version='2.0.0',
     description='A library containing the conclib packages.',
     author='',
