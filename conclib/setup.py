0050801aad0ba319027115720bd7c33d05a9786f
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-10-20 17:40:35 +1100

    update package info

--- conclib/setup.py
+++ conclib/setup.py
@@ -2,12 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='conclib',
-    version='2.0.0',
+    version='2.0.1',
     description='A library containing the conclib packages.',
     author='',
     author_email='',
     url='https://github.com/uaineteine/concordance_lib',
     packages=find_packages(include=['conclib', 'conclib.*', 'rkeylib', 'rkeylib.*']),
+        install_requires=[
+        'pyspark',
+        'numpy'
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
--- rkeylib/setup.py
+++ rkeylib/setup.py
@@ -2,12 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='rkeylib',
-    version='2.0.0',
+    version='2.0.1',
     description='A library containing the rkeylib packages.',
     author='',
     author_email='',
     url='https://github.com/uaineteine/concordance_lib',
     packages=find_packages(include=['conclib', 'conclib.*', 'rkeylib', 'rkeylib.*']),
+        install_requires=[
+        'pyspark',
+        'conclib',
+        'hash_method>=2.0.2'
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
